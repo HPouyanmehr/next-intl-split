0c7228648bca88ddab088911f32c8e8b60591448
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2024-09-12 20:43:46 +0330

    fix: Clear git cache

--- package/load/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const loadI18nTranslations: (dictionariesPath: string, locale: string) => any;
-export declare const loadMessages: (dictionariesPath: string) => {
-    [key: string]: any;
-};
--- package/write/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const writeMessages: (dictionariesPath: string, messages: {}) => void;
